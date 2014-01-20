380d45207b18cdcd4326e9074e35383b81893a51
bradsimpson <bradley.simpson@gmail.com>
2014-01-20 13:36:51 -0800

    second visual

--- js/visual_2.js
+++ js/visual_2.js
@@ -12,8 +12,14 @@ function setPosAndLED(valueStore){
 
 			//m1 = 0.01
 			//m1 = 0.033
-			valueStore.pos[x][y] = noise.perlin3(1, (x+1) * m1, (y+1) * m1) * Math.sin( t/10000 + (x) * (y));
-			valueStore.led[x][y] = valueStore.pos[x][y];
+			var mult = 1;
+			if ( x % 2 == true ) {
+				mult = -1; 
+			}
+			
+			valueStore.pos[x][y] =  0.5*Math.sin( ((y+1)/h)*mult*t/10000  + (x/w));
+			//valueStore.pos[x][y] = noise.perlin3(1, (x+1) * m1, (y+1) * m1) * Math.sin( t/10000 + (x) * (y));
+			valueStore.led[x][y] = 0.2 + valueStore.pos[x][y];
 			//valueStore.led[x][y] = 0.1 + Math.abs( noise.perlin3(1, y * m1, x * m1) * Math.sin( t/10000 + x * y) );	
 
 		}
