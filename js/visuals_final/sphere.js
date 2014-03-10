39394c1847a64ba512b7cc5d1073d058dcf660df
bradsimpson <bradley.simpson@gmail.com>
2014-03-09 20:13:16 -0700

    sphere

--- js/visuals_final/sphere.js
+++ js/visuals_final/sphere.js
@@ -1,16 +1,5 @@
 (function(module){
 
-	var currentSide = 0;
-	bulb_led_arr = new Array(10);
-
-	for(var a = 0; a < 10; a++){
-		bulb_led_arr[a] = new Array(10);
-	}
-
-	bulb_init = false;
-
-	var height = 1;
-	
 
 
 //awesome sinusoidal wave that finds harmonics
@@ -26,13 +15,7 @@ function setPosAndLED(valueStore){
 
 	var timeMultiplier = 5000;
 		
-	if(!bulb_init){
-		for(var x = 0; x < w; x++){
-			for(var y = 0; y < h; y++){
-			}
-		}
-		bulb_init = true;
-	}
+	
 
 	for(var x=0; x<5; x++){
 		for(var y=0; y<5; y++){
@@ -40,24 +23,20 @@ function setPosAndLED(valueStore){
 			//var tx = x*(1 + Math.sin(t/timeMultiplier))/2;
 			//var ty = y*(1 + Math.sin(t/timeMultiplier))/2;
 			var mult = 1;
-				if ( x % 2 == true ) {
-					mult = -1; 
-				}
-
+			if ( x % 2 == true ) {
+				mult = -1; 
+			}
 
 			var zPos = Math.sqrt(Math.pow(0.5,2) - Math.pow((x/8)-0.5,2) - Math.pow((y/8)-0.5,2));
 			valueStore.pos[x][y] = mult*zPos;
-
-			//valueStore.pos[x][y] *= -1;
-			//valueStore.pos[x][y] += 1;
-
 			valueStore.pos[(w-1)-x][y] = valueStore.pos[x][y];
 			valueStore.pos[x][(h-1)-y] = valueStore.pos[x][y];
 			valueStore.pos[(w-1)-x][(h-1)-y] = valueStore.pos[x][y];
 
-
-
-			valueStore.led[x][y] = 1-valueStore.pos[x][y];
+			//valueStore.pos[x][y] *= -1;
+			//valueStore.pos[x][y] += 1;
+			var ledPos = (1 + Math.sin(t/timeMultiplier))/2;
+			valueStore.led[x][y] = Math.abs(valueStore.pos[x][y] - ledPos);
 			valueStore.led[(w-1)-x][y] = valueStore.led[x][y];
 			valueStore.led[x][(h-1)-y] = valueStore.led[x][y];
 			valueStore.led[(w-1)-x][(h-1)-y] = valueStore.led[x][y];
