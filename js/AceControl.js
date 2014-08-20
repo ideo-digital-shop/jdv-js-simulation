7658aee057623bfeebae03c5f363d69c8e015fcd
Aaron Soloway <asoloway@pa-m-loaner12.ideo.com>
2014-08-20 13:39:08 -0700

    Add Ace web code environment

---
+++ js/AceControl.js
@@ -0,0 +1,24 @@
+(function(module){
+    function runCode(doc){
+
+    	//js eval
+    	
+    	
+    
+
+    }
+    // does code conform to JS standards?
+    function checkCode(doc){
+
+    }
+
+    // does code conform to height delta restrictions?
+    function codeBreaksEdison(doc){
+
+    }
+    if(module){
+        module.exports.runCode = runCode;
+        module.exports.checkCode = checkCode;
+        module.exports.codeBreaksEdison = codeBreaksEdison;
+    }
+})(module);
